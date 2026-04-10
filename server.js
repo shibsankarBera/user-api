cc866e41a72f2acd08a2feb5057c2afcc50a0ec1
shibsankarBera <shibu900099@gmail.com>
2026-04-11 02:22:20 +0530

    add GET routes for users

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const users = require("./data/users");
 const app = express();
 
 app.use(express.json());
@@ -10,4 +11,21 @@ app.get("/", (req, res) => {
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+
+
+// GET all users
+app.get("/users", (req, res) => {
+    res.status(200).json(users);
+});
+
+// GET user by ID
+app.get("/users/:id", (req, res) => {
+    const user = users.find(u => u.id === req.params.id);
+
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
 });
